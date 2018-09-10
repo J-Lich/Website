44ed83cf14e25208180b740c989ddd0f8a4f37e6
J-Lich <James.LIcciardi@gmail.com>
2018-09-11 09:16:30 +0930

    Adding Website

---
+++ js/overlay.js
@@ -0,0 +1,20 @@
+function openOverlay() {
+	document.getElementById("menuoverlay").style.display = "block";
+	
+	document.getElementById("menubutton").style.display = "none";
+	document.getElementById("closemenubutton").style.display = "block";
+}
+
+function closeOverlay() {
+	document.getElementById("menuoverlay").style.display = "none";
+	
+	document.getElementById("menubutton").style.display = "block";
+	document.getElementById("closemenubutton").style.display = "none";
+	
+	
+	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+	if (width > 670) {
+		document.getElementById("menubutton").style.display = "none";
+	}
+}
