ebe2d12224959b02277fe40a790ca37decc5210a
J-Lich <James.LIcciardi@gmail.com>
2018-09-20 11:50:14 +0930

    Menu Overlay Edits

--- js/overlay.js
+++ js/overlay.js
@@ -1,17 +1,17 @@
 function openOverlay() {
 	document.getElementById("menuoverlay").style.display = "block";
-	
+
 	document.getElementById("menubutton").style.display = "none";
 	document.getElementById("closemenubutton").style.display = "block";
 }
 
 function closeOverlay() {
 	document.getElementById("menuoverlay").style.display = "none";
-	
+
 	document.getElementById("menubutton").style.display = "block";
 	document.getElementById("closemenubutton").style.display = "none";
-	
-	
+
+
 	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
 	if (width > 670) {
