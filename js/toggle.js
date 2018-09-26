5249eb237a11e7043caaa11bae4c2a9a5fb36c0e
J-Lich <James.LIcciardi@gmail.com>
2018-09-26 13:17:23 +0930

    Lots of new changes but finally a website.

---
+++ js/toggle.js
@@ -0,0 +1,29 @@
+function toggle() {
+  document.getElementById('toggle').classList.toggle("hide")
+}
+
+function toggle1() {
+  document.getElementById('toggle1').classList.toggle("hide")
+}
+
+function toggle2() {
+  document.getElementById('toggle2').classList.toggle("hide")
+}
+
+function toggle3() {
+  document.getElementById('toggle3').classList.toggle("hide")
+}
+function toggle4() {
+  document.getElementById('toggle4').classList.toggle("hide")
+}
+
+function toggle5() {
+  document.getElementById('toggle5').classList.toggle("hide")
+}
+function toggle6() {
+  document.getElementById('toggle6').classList.toggle("hide")
+}
+
+function toggle7() {
+  document.getElementById('toggle7').classList.toggle("hide")
+}
