5abc0c799b7a52d758857e3b48cb8e3b33641c11
James MacbookPro <james.licciardi@gmail.com>
2019-01-28 09:11:11 +1030

    Removal of more info sections

--- js/toggle.js
+++ js/toggle.js
@@ -1,29 +1,31 @@
 function toggle() {
   document.getElementById('toggle').classList.toggle("hide")
+
+  document.getElementById('toggle1').classList.add("hide")
+  document.getElementById('toggle2').classList.add("hide")
+  document.getElementById('toggle3').classList.add("hide")
 }
 
 function toggle1() {
   document.getElementById('toggle1').classList.toggle("hide")
+
+  document.getElementById('toggle').classList.add("hide")
+  document.getElementById('toggle2').classList.add("hide")
+  document.getElementById('toggle3').classList.add("hide")
 }
 
 function toggle2() {
   document.getElementById('toggle2').classList.toggle("hide")
+
+  document.getElementById('toggle').classList.add("hide")
+  document.getElementById('toggle1').classList.add("hide")
+  document.getElementById('toggle3').classList.add("hide")
 }
 
 function toggle3() {
   document.getElementById('toggle3').classList.toggle("hide")
-}
-function toggle4() {
-  document.getElementById('toggle4').classList.toggle("hide")
-}
 
-function toggle5() {
-  document.getElementById('toggle5').classList.toggle("hide")
-}
-function toggle6() {
-  document.getElementById('toggle6').classList.toggle("hide")
-}
-
-function toggle7() {
-  document.getElementById('toggle7').classList.toggle("hide")
+  document.getElementById('toggle').classList.add("hide")
+  document.getElementById('toggle1').classList.add("hide")
+  document.getElementById('toggle2').classList.add("hide")
 }
